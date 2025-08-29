299918b3057822274800ed5874e5d37a533ec214
Rafael Curi Leonardo <rafaelcuri@cbpf.br>
2025-08-29 15:20:50 -0300

    ✨ adicionado subpacote router

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/rafinhacuri/api-go.git/router"
+)
+
+func main() {
+	router.InitializeRouter(":8080")
+}
---
+++ router/router.go
@@ -0,0 +1,12 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func InitializeRouter(port string) {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) { c.JSON(200, gin.H{"message": "pong"}) })
+
+	router.Run(port)
+}
