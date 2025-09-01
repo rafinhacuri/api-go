f77427cb7dfe8724cfc79e7f7c802f6afad57ff9
Rafael Curi Leonardo <rafaelcuri@cbpf.br>
2025-09-01 14:47:56 -0300

    ✨ add get

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -4,8 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rafinhacuri/api-go.git/schemas"
 )
 
 func GetOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"message": "Openiiiiing Get"})
+	id := ctx.Query("id")
+
+	if id == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "ID is required"})
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"error": "Opening not found"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"opening": opening})
 }
