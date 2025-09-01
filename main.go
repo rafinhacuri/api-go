d8b63edb015e5fbc05ce75db68a9414e2f719d0c
Rafael Curi Leonardo <rafaelcuri@cbpf.br>
2025-09-01 11:29:49 -0300

    ✨ add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"github.com/rafinhacuri/api-go.git/config"
 	"github.com/rafinhacuri/api-go.git/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Error initializing the application: %v", err)
+		return
+	}
+
 	router.InitializeRouter(":8080")
 }
